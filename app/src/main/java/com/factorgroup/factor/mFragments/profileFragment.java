aef296f6a8ce40c5f11f5b2029ed1addd4b931b3
PardeepS2 <Pardeemaloya@gmail.com>
2017-10-08 14:28:35 -0400

    fix error in profile fragment

--- app/src/main/java/com/factorgroup/factor/mFragments/profileFragment.java
+++ app/src/main/java/com/factorgroup/factor/mFragments/profileFragment.java
@@ -2,13 +2,14 @@ package com.factorgroup.factor.mFragments;
 
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.factorgroup.factor.R;
 
-public class profileFragment {
+public class profileFragment extends Fragment {
 
     @Nullable
     @Override
