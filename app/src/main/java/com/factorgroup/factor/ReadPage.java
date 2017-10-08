67b1e4d6d0df2e43297db69b74307ce3f23213b3
PardeepS2 <Pardeemaloya@gmail.com>
2017-10-08 11:52:03 -0400

    Initial commit

---
+++ app/src/main/java/com/factorgroup/factor/ReadPage.java
@@ -0,0 +1,13 @@
+package com.factorgroup.factor;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ReadPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_read_page);
+    }
+}
